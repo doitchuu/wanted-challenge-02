aa887249f7a03515611bdc1708575ef2022d88be
doitchuu <seulgichu14@gmail.com>
2024-12-31 02:22:04 +0900

    fix: App router 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import ProtectedLayout from "./layouts/ProtectedLayout";
 
-import AuthPage from "./pages/AuthPage";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import SignInPage from "./pages/SignInPage";
 import TodoListPage from "./pages/TodoListPage";
 import TodoDetailPage from "./pages/TodoDetailPage";
-import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
+      <Route element={<DefaultLayout />}>
         <Route path="/" element={<MainPage />} />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signin" element={<SignInPage />} />
       </Route>
 
-      <Route path="/todo" element={<ProtectedLayout />}>
-        <Route path="/" element={<TodoListPage />} />
-        <Route path="/:id" element={<TodoDetailPage />} />
+      <Route element={<ProtectedLayout />}>
+        <Route path="/todo" element={<TodoListPage />} />
+        <Route path="/todo/:id" element={<TodoDetailPage />} />
       </Route>
     </Routes>
   );
