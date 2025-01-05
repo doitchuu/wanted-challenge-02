bf94edcfa7755285b11a3aa99064ffc5228726f3
doitchuu <seulgichu14@gmail.com>
2025-01-06 01:25:26 +0900

    fix: 로그인 필요 페이지 시 로그인페이지로 리다이렉트 로직 추가

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,10 +1,23 @@
-import { Outlet, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, Outlet, Link } from "react-router-dom";
 
 function ProtectedLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      alert("로그인이 필요해요. 로그인 후에 이용해주세요 🥲");
+      navigate("/login");
+    }
+  }, [navigate]);
+
   return (
     <div>
       <header>
         <Link to="/">Logo</Link>
+        <Link to="/todo">Todo</Link>
       </header>
       <main>
         <Outlet />
