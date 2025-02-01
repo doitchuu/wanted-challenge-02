d6f4743184a2793e6d40988e2adc03195fb09970
doitchuu <seulgichu14@gmail.com>
2025-02-01 18:07:51 +0900

    fix: Button props 수정

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -3,20 +3,24 @@ import clsx from "clsx";
 interface ButtonProps {
   type: "button" | "submit" | "reset";
   disabled?: boolean;
+  onClick?: () => void;
   children: React.ReactNode;
+  className?: string;
 }
 
-function Button({ type, disabled, children }: ButtonProps) {
+function Button({ type, disabled, onClick, children, className }: ButtonProps) {
   return (
     <button
       type={type}
       disabled={disabled}
+      onClick={onClick}
       className={clsx(
-        "w-full h-12 mt-4 text-white rounded-md text-sm font-medium",
+        "h-12 mt-4 text-white rounded-md text-sm font-medium",
         {
           "bg-blue-500": !disabled,
           "bg-slate-400": disabled,
-        }
+        },
+        className
       )}
     >
       {children}
